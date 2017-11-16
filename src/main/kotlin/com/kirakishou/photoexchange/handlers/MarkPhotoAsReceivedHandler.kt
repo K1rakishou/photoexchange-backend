a868ee554c0a55920d0c5c50b5fc72f9ef3e2a84
kirakishou <kez1911@protonmail.com>
2017-11-16 16:28:03 +0300

    MarkPhotoAsReceivedHandler rewritten with coroutines

--- src/main/kotlin/com/kirakishou/photoexchange/handlers/MarkPhotoAsReceivedHandler.kt
+++ src/main/kotlin/com/kirakishou/photoexchange/handlers/MarkPhotoAsReceivedHandler.kt
@@ -1,8 +1,13 @@
 package com.kirakishou.photoexchange.handlers
 
+import com.kirakishou.photoexchange.model.ServerErrorCode
 import com.kirakishou.photoexchange.model.net.response.StatusResponse
+import com.kirakishou.photoexchange.model.net.response.UploadPhotoResponse
 import com.kirakishou.photoexchange.repository.PhotoInfoRepository
 import com.kirakishou.photoexchange.service.JsonConverterService
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.reactor.asMono
 import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -18,40 +23,37 @@ class MarkPhotoAsReceivedHandler(
     private val USER_ID_PATH_VARIABLE = "user_id"
 
     override fun handle(request: ServerRequest): Mono<ServerResponse> {
-        /*//TODO: check USER_ID_PATH_VARIABLE existence
-        val photoIdString = request.pathVariable(PHOTO_ID_PATH_VARIABLE)
-        val userId = request.pathVariable(USER_ID_PATH_VARIABLE)
+        val result = async {
+            try {
+                //TODO: check USER_ID_PATH_VARIABLE existence
+                val photoIdString = request.pathVariable(PHOTO_ID_PATH_VARIABLE)
+                val userId = request.pathVariable(USER_ID_PATH_VARIABLE)
 
-        val photoId = try {
-            photoIdString.toLong()
-        } catch (e: NumberFormatException) {
-            -1L
-        }
-
-        if (photoId == -1L) {
-            return formatResponse(HttpStatus.BAD_REQUEST, StatusResponse.from(ServerErrorCode.BAD_PHOTO_ID))
-        }
-
-        val updateResultFlux = photoInfoRepo.updateSetPhotoSuccessfullyDelivered(photoId, userId)
-                .flux()
-                .share()
-
-        val wasUpdatedMono = updateResultFlux
-                .filter { wasUpdated -> wasUpdated }
-                .flatMap {
-                    formatResponse(HttpStatus.OK, StatusResponse.from(ServerErrorCode.OK))
+                val photoId = try {
+                    photoIdString.toLong()
+                } catch (e: NumberFormatException) {
+                    -1L
                 }
 
-        val wasNotUpdatedMono = updateResultFlux
-                .filter { wasUpdated -> !wasUpdated }
-                .flatMap {
-                    formatResponse(HttpStatus.INTERNAL_SERVER_ERROR, StatusResponse.from(ServerErrorCode.UNKNOWN_ERROR))
+                if (photoId == -1L) {
+                    return@async formatResponse(HttpStatus.BAD_REQUEST, StatusResponse.from(ServerErrorCode.BAD_PHOTO_ID))
                 }
 
-        return Flux.merge(wasUpdatedMono, wasNotUpdatedMono)
-                .single()*/
+                if (!photoInfoRepo.updateSetPhotoSuccessfullyDelivered(photoId, userId)) {
+                    return@async formatResponse(HttpStatus.INTERNAL_SERVER_ERROR, StatusResponse.from(ServerErrorCode.UNKNOWN_ERROR))
+                }
 
-        TODO()
+                return@async formatResponse(HttpStatus.OK, StatusResponse.from(ServerErrorCode.OK))
+
+            } catch (error: Throwable) {
+                error.printStackTrace()
+                return@async formatResponse(HttpStatus.INTERNAL_SERVER_ERROR, UploadPhotoResponse.fail(ServerErrorCode.UNKNOWN_ERROR))
+            }
+        }
+
+        return result
+                .asMono(CommonPool)
+                .flatMap { it }
     }
 
     private fun formatResponse(httpStatus: HttpStatus, response: StatusResponse): Mono<ServerResponse> {
