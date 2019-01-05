4c39b6f126d408b911ff380d856a41ffcd7f58a5
k1rakishou <kez1911@protonmail.com>
2019-01-05 18:16:48 +0300

    Add test for countFreshUploadedPhotosSince method

---
+++ src/test/kotlin/com/kirakishou/photoexchange/database/dao/PhotosDaoTest.kt
@@ -0,0 +1,29 @@
+package com.kirakishou.photoexchange.database.dao
+
+import com.kirakishou.photoexchange.AbstractTest
+import com.kirakishou.photoexchange.core.UserId
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+
+class PhotosDaoTest : AbstractTest() {
+
+  @Before
+  override fun setUp() {
+    super.setUp()
+  }
+
+  @After
+  override fun tearDown() {
+    super.tearDown()
+  }
+
+  @Test
+  fun `test countFreshUploadedPhotosSince should return 0 when DB is empty`() {
+    dbQuery {
+      assertEquals(0, photosDao.countFreshUploadedPhotosSince(UserId(1), 1000))
+    }
+  }
+
+}
